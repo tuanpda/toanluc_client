6823b8a7f00daca58a694d9d05b8d76782ec9479
tuanpda <tuanpda.bhxh@gmail.com>
2024-11-11 15:01:20 +0700

    commit by tuanpda

--- nuxtAuthSchema/CustomScheme.js
+++ nuxtAuthSchema/CustomScheme.js
@@ -1,4 +1,4 @@
-import { LocalScheme } from '~auth/runtime'
+import { LocalScheme } from "~auth/runtime";
 // import { LocalScheme } from '@nuxtjs/auth'
 
 export default class CustomScheme extends LocalScheme {
@@ -9,40 +9,39 @@ export default class CustomScheme extends LocalScheme {
     // const response = await this.$auth.requestWith(this.name, endpoint)
 
     // Token is required but not available
-    // console.log(this.check());
+    console.log(this.check());
     if (!this.check().valid) {
-      return
+      return;
     }
 
     // User endpoint is disabled.
     if (!this.options.endpoints.user) {
-      this.$auth.setUser({})
-      return
+      this.$auth.setUser({});
+      return;
     }
 
     // Try to fetch user and then set
-    return this.$auth.requestWith(
-      this.name,
-      endpoint,
-      this.options.endpoints.user
-    ).then((response) => {
-      // console.log(response.data);
-      const user = {
-        email: response.data.email,
-        username: response.data.username,
-        name: response.data.name,
-        role: response.data.role,
-        avatar: response.data.avatar,
-      };
+    return this.$auth
+      .requestWith(this.name, endpoint, this.options.endpoints.user)
+      .then((response) => {
+        // console.log(response.data);
+        const user = {
+          email: response.data.email,
+          username: response.data.username,
+          name: response.data.name,
+          role: response.data.role,
+          avatar: response.data.avatar,
+        };
 
-      // Set the custom user
-      // The `customUser` object will be accessible through `this.$auth.user`
-      // Like `this.$auth.user.fullName` or `this.$auth.user.roles`
-      this.$auth.setUser(user)
+        // Set the custom user
+        // The `customUser` object will be accessible through `this.$auth.user`
+        // Like `this.$auth.user.fullName` or `this.$auth.user.roles`
+        this.$auth.setUser(user);
 
-      return response
-    }).catch((error) => {
-      this.$auth.callOnError(error, { method: 'fetchUser' })
-    })
+        return response;
+      })
+      .catch((error) => {
+        this.$auth.callOnError(error, { method: "fetchUser" });
+      });
   }
 }
